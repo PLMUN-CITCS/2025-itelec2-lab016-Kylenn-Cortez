994fe931e1ee6e68b30bd39abf0008ccb2a71cab
Kylenn-Cortez <kylennrhysscortez_bsit@plmun.edu.ph>
2025-02-18 16:23:34 +0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,12 @@
+# Kylenn Rhyss Cortez
+# ITELEC2
+# Laboratory # 16 - Guided Coding Exercise: Basic Function Definition and Calling
+
+def greet():
+    """Prints a greeting message."""  
+
+    print("Hello, Python Students!")
+
+
+greet()
+
